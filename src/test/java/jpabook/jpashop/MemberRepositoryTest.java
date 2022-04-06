a84e3b1512249bc1406e5f788a8b1878892b1c2a
jack <jack68@naver.com>
2022-04-06 13:12:00 +0900

    first commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+}
