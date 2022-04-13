d3e0aaa52c21874af25e3796d47a33750f354891
jack <jack68@naver.com>
2022-04-13 17:34:07 +0900

    member repository test code

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,16 +1,35 @@
 package jpabook.jpashop;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class MemberRepositoryTest {
 
-    @Test
-    void save() {
-    }
+    @Autowired
+    MemberRepository repository;
 
     @Test
-    void find() {
+    @Transactional
+    @Rollback(false)
+    void saveTest() {
+        Member member = new Member();
+        member.setUsername("MemberA");
+
+        Long saveId = repository.save(member);
+        Member findMember = repository.find(saveId);
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
     }
 }
