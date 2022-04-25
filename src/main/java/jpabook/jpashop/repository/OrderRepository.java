3a3c339ea690c3b6493b05f0a83f14a1dde5ab31
jkkim74 <jack68@naver.com>
2022-04-26 00:16:12 +0900

    querydsl add

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,11 +1,22 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,5 +32,18 @@ public class OrderRepository {
         return em.find(Order.class,id);
     }
     // 검색용...
-    // public List<Order> findAll(OrderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery ("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name",Order.class)
+                .setParameter ("status",orderSearch.getOrderStatus())
+                .setParameter ("name", orderSearch.getMemberName ())
+                .getResultList ();
+//        QOrder qOrder = QOrder.order;
+//        QOrder member = new QOrder("member");
+//        JPAQuery<?> query = new JPAQuery<Void> (em);
+//        Predicate joinMethon = qOrder.member.eq (member.member).and (qOrder.member.);
+//        query.select (qOrder).from (qOrder).join(member).on (joinMethon);
+
+    }
 }
